8548a7d05a657abdf41b63b21e4907b229d77e55
LeeJiSun <j5119s@naver.com>
2019-06-07 23:02:32 +0900

    no complete

--- B2110.java
+++ B2110.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class B2110 {
     public static void main(String[] args) {
+        int N, C;
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        C = sc.nextInt();
+
 
     }
 }
